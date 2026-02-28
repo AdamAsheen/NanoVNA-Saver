a3a53ac4cb867cb2976e5267005b05b48ad72b5b
Adam Asheen <Adam@Asheen.uk>
2026-02-28 09:37:18 +0000

    Format the main.rs file
    Used cargo fmt to format the rust file main.rs to ensure it passes the
    formatting checks
    
    issue #76

--- NanoVNA-Saver/src/main.rs
+++ NanoVNA-Saver/src/main.rs
@@ -1,15 +1,14 @@
-use std::thread;
 use clap::Parser;
-use std::path::PathBuf;
 use polars::prelude::{CsvWriter, SerWriter};
 use std::fs::File;
+use std::path::PathBuf;
+use std::thread;
 mod sweep;
 
 #[derive(Parser, Debug)]
 #[command(name = "nanovna-saver")]
 #[command(about = "Performs NanoVNA sweeps with configurable parameters")]
 struct Args {
-
     #[arg(long, default_value_t = 1)]
     num_sweeps: usize,
 
@@ -33,56 +32,53 @@ struct Args {
 
     #[arg(long)]
     path: Option<PathBuf>,
-
 }
 
 fn main() {
-
     let args = Args::parse();
 
     let output_path = args.path.unwrap_or_else(|| {
-    std::env::current_dir()
-        .expect("Failed to get current working directory").join("output.csv")
+        std::env::current_dir()
+            .expect("Failed to get current working directory")
+            .join("output.csv")
     });
 
     let Args {
-    num_sweeps,
-    vna_number,
-    start_freq,
-    end_freq,
-    mut num_points,
-    num_ports,
-    if_bandwidth,
-    ..
+        num_sweeps,
+        vna_number,
+        start_freq,
+        end_freq,
+        mut num_points,
+        num_ports,
+        if_bandwidth,
+        ..
     } = args;
 
-        // Limit num_points to 101 if more are typed
+    // Limit num_points to 101 if more are typed
     if num_points > 101 {
         println!("num_points limited to 101 (was {})", num_points);
         num_points = 101;
     }
 
-    let ports = tokio_serial::available_ports()
-        .expect("Failed to enumerate serial ports");
+    let ports = tokio_serial::available_ports().expect("Failed to enumerate serial ports");
 
     if ports.is_empty() {
         eprintln!("No VNAs found");
         return;
     }
 
-
     // Checks if the serial port is connected
-    let vnas_to_use = ports
-        .into_iter()
-        .take(vna_number);
+    let vnas_to_use = ports.into_iter().take(vna_number);
     // Print line for table header
-    println!("| ID | Label | VNA NUMBER | TIME COMMAND SENT | TIME READING RECEIVED | Frequency | SParameter | Real | Imaginary |");
+    println!(
+        "| ID | Label | VNA NUMBER | TIME COMMAND SENT | TIME READING RECEIVED | Frequency | SParameter | Real | Imaginary |"
+    );
 
     let mut handles = Vec::new();
 
     for (idx, port) in vnas_to_use.enumerate() {
         let port_name = port.port_name.clone();
-        let vna_number = idx + 1; 
+        let vna_number = idx + 1;
 
         let params = sweep::SweepParams {
             port_name,
@@ -94,9 +90,7 @@ fn main() {
             num_ports,
             if_bandwidth,
         };
-        let handle = thread::spawn(move || {
-            sweep::run_on_port(params)
-        });
+        let handle = thread::spawn(move || sweep::run_on_port(params));
 
         handles.push(handle);
     }
@@ -112,19 +106,17 @@ fn main() {
     let mut final_df = iter.next().expect("No data collected");
 
     for df in iter {
-        final_df.vstack_mut(&df).expect("Failed to stack DataFrames");
+        final_df
+            .vstack_mut(&df)
+            .expect("Failed to stack DataFrames");
     }
 
-    
-    let mut file = File::create(&output_path)
-    .expect("Failed to create CSV file");
+    let mut file = File::create(&output_path).expect("Failed to create CSV file");
 
     CsvWriter::new(&mut file)
-    .include_header(true)
-    .finish(&mut final_df)
-    .expect("Failed to write CSV");
+        .include_header(true)
+        .finish(&mut final_df)
+        .expect("Failed to write CSV");
 
     println!("Saved CSV to {:?}", output_path);
-
 }
-
