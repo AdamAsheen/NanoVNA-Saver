28016e348fcc074f7afcde2fed9376d942ad893a
Adam Asheen <Adam@Asheen.uk>
2026-03-03 17:21:40 +0000

    Add configuration struct
    Configuration struct allows it to be called by both clap and the
    frontend
    
    issue #85

--- NanoVNA-Saver/src/lib.rs
+++ NanoVNA-Saver/src/lib.rs
@@ -4,4 +4,17 @@ use tokio_serial::SerialPortType;
 
 pub mod sweep;
 
-use swee::SweepParams;
+use sweep::SweepParams;
+
+pub struct RunConfig {
+    pub num_sweeps: usize,
+    pub vna_number: usize,
+    pub start_freq: u64,
+    pub end_freq: u64,
+    pub num_points: usize,
+    pub num_ports: usize,
+    pub if_bandwidth: Option<u32>,
+    pub time: Option<u64>,
+    pub label: String,
+    pub no_print: bool,
+}
